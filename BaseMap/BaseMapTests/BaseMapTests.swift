b87c32021268fc2d0dbf7cb0fd4394a7a000b5b2
Samip Neupane <sneupan3@uno.edu>
2017-06-11 02:13:27 -0500

    Commit

---
+++ BaseMap/BaseMapTests/BaseMapTests.swift
@@ -0,0 +1,36 @@
+//
+//  BaseMapTests.swift
+//  BaseMapTests
+//
+//  Created by Samip Neupane on 6/10/17.
+//  Copyright © 2017 Samip Neupane. All rights reserved.
+//
+
+import XCTest
+@testable import BaseMap
+
+class BaseMapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
